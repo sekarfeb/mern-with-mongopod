239c32e0e7cb2a092072c640a5c26018fc3f612a
sekarfeb <sekarfeb@gmail.com>
2023-07-17 13:32:02 +0530

    firr

--- frontend/src/context/appContext.js
+++ frontend/src/context/appContext.js
@@ -72,7 +72,7 @@ const AppProvider = ({ children }) => {
 
   // axios
   const authFetch = axios.create({
-    baseURL: '/api/v1',
+    baseURL: 'http://34.27.34.192:5000/api/v1',
   });
   // request
 
