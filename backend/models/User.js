0ab009f6b96dc6ef59e6782ad9c447cc22faa0c9
sekarfeb <sekarfeb@gmail.com>
2023-07-19 13:10:37 +0530

    changes

--- backend/models/User.js
+++ backend/models/User.js
@@ -46,6 +46,7 @@ UserSchema.pre('save', async function () {
   this.password = await bcrypt.hash(this.password, salt)
 })
 
+// Jwt Lifetime value
 UserSchema.methods.createJWT = function () {
   return jwt.sign({ userId: this._id }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_LIFETIME,
